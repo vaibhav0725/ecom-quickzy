776ae9ea57a1bf723a1b703d9af76c5a9c52d6cf
vaibhav0725 <vaibhavwork0725@gmail.com>
2025-06-12 17:04:34 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Container } from "@/components/container";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 
 const inter = Inter({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
