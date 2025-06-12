562ac69137972ef3485d305fa2d333eace93b466
vaibhav0725 <vaibhavwork0725@gmail.com>
2025-06-12 17:24:37 +0530

    update

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,7 +1,13 @@
 import { FeaturedProducts } from "@/components/featured-products";
 import { Product } from "@/components/product";
 
-export default async function ProductPage({ params }: { params: { slug: string } }) {
+interface PageProps {
+    params: {
+      slug: string;
+    };
+  }
+
+export default async function ProductPage({ params }:PageProps) {
     return (
         <div className="w-full h-full">
             <Product slug={params.slug}/>
